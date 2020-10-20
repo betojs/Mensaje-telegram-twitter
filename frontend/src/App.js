a184b75d94b0d5c317e38295c1b75db483eac9e1
betojs <albertoj2504@gmail.com>
2020-10-20 10:06:08 -0400

    Frontend created

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Mensaje al telegran de una notificacion del twitter
+    </h1>
+    </div>
+  );
+}
+
+export default App;
